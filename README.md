7ef61a030deaee95e0089e7445399e206fcb4ce4
termer <termer@termer.net>
2023-04-02 02:49:37 -0400

    further experiments and archival

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nim-asyncstream-core
 Unified asynchronous stream interface in Nim
+
+# Archived
+Nim generics and concepts do not mix, and it's nearly impossible to implement a concept with generic parameters.
+I'm archiving this so it can be used for reference, but as of now, there's no way to make it work.
--- src/asyncstream_core/dummy.nim
+++ src/asyncstream_core/dummy.nim
@@ -3,32 +3,29 @@
 import std/[asyncdispatch, options]
 import ".."/[asyncstream_core]
 
-type DummyAsyncReadStream*[T] = ref object
+type DummyAsyncReadStream* = ref object
   intIsFinished: bool
 
-proc read[T](this: DummyAsyncReadStream[T]): Future[ReadResult[T]]
+proc read(this: DummyAsyncReadStream): Future[ReadResult[string]]
   ##
 
-proc read[T](this: DummyAsyncReadStream[T], count: int): Future[ReadResult[seq[T]]]
+proc read[T](this: DummyAsyncReadStream, count: int): Future[ReadResult[seq[string]]]
   ##
 
-proc readAll[T](this: DummyAsyncReadStream[T]): Future[ReadResult[seq[T]]]
+proc readAll[T](this: DummyAsyncReadStream): Future[ReadResult[seq[string]]]
   ##
 
-proc pipeTo[T](this: DummyAsyncReadStream[T], writeStream: AsyncWriteStream[T]): Future[PipeResult]
+func isFinished*[T](this: DummyAsyncReadStream): bool
   ##
 
-func isFinished*[T](this: DummyAsyncReadStream[T]): bool
-  ##
-
-func isFailed*[T](this: DummyAsyncReadStream[T]): bool
+func isFailed*[T](this: DummyAsyncReadStream): bool
   ## 
 
-func canRead*[T](this: DummyAsyncReadStream[T]): bool
+func canRead*[T](this: DummyAsyncReadStream): bool
   ## 
 
-func exception*[T](this: DummyAsyncReadStream[T]): Option[Exception]
+func exception*[T](this: DummyAsyncReadStream): Option[Exception]
   ## The exception that caused the stream to fail, or None if the stream did not fail.
   ## Must be Some if isFailed is true.
 
-DummyAsyncReadStream.implements(AsyncReadStream)
+DummyAsyncReadStream.implements(AsyncReadStream[string])
